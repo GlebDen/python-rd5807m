83d5fd4a1feef74221f3cb50d7f89f3658ade0de
franckinux <contact@franck-barbenoire.fr>
2016-03-19 16:51:35 +0100

    factorize writing methods

--- rda5807m.py
+++ rda5807m.py
@@ -55,28 +55,34 @@ RDA_RDS_EN = 0b0000000000001000
 RDA_NEW_METHOD = 0b0000000000000100
 RDA_SOFT_RESET = 0b0000000000000010
 RDA_ENABLE = 0b0000000000000001
+
 # REG 0x03
 RDA_CHAN = 0b1111111111000000
 RDA_DIRECT_MODE = 0b0000000000100000
 RDA_TUNE = 0b0000000000010000
 RDA_BAND = 0b0000000000001100
 RDA_SPACE = 0b0000000000000011
+
 # REG 0x04
 RDA_DE = 0b0000100000000000
 RDA_SOFTMUTE_EN = 0b0000001000000000
 RDA_AFCD = 0b0000000100000000
+
 # REG 0x05
 RDA_INT_MODE = 0b1000000000000000
 RDA_SEEKTH = 0b0000111100000000
 RDA_VOLUME = 0b0000000000001111
+
 # REG 0x06
 RDA_OPEN_MODE = 0b0110000000000000
+
 # REG 0x07
 RDA_BLEND_TH = 0b0111110000000000
 RDA_65_50M_MODE = 0b0000001000000000
 RDA_SEEK_TH_OLD = 0b0000000011111100
 RDA_BLEND_EN = 0b0000000000000010
 RDA_FREQ_MODE = 0b0000000000000001
+
 # REG 0x0A
 RDA_RDSR = 0b1000000000000000
 RDA_STC = 0b0100000000000000
@@ -85,6 +91,7 @@ RDA_RDSS = 0b0001000000000000
 RDA_BLK_E = 0b0000100000000000
 RDA_ST = 0b0000010000000000
 RDA_READCHAN = 0b0000001111111111
+
 # REG 0x0B
 RDA_RSSI = 0b1111110000000000
 RDA_FM_TRUE = 0b0000001000000000
@@ -160,67 +167,29 @@ class Rda5807m:
         self.out_buffer[10] = data >> 8
         self.out_buffer[11] = data & 0xff
 
-    def write_off(self):
-        data = (self.read_chip(2) | RDA_ENABLE) ^ RDA_ENABLE
-        self.out_buffer[0] = data >> 8
-        self.out_buffer[1] = data & 0xff
-
-    def write_dmute(self, value):
-        data = (self.read_chip(2) | RDA_DMUTE)
-        if value == 0:
-            data = data ^ RDA_DMUTE
-        self.out_buffer[0] = data >> 8
-        self.out_buffer[1] = data & 0xff
-
-    def write_mono(self, value):
-        data = (self.read_chip(2) | RDA_MONO)
-        if value == 0:
-            data = data ^ RDA_MONO
-        self.out_buffer[0] = data >> 8
-        self.out_buffer[1] = data & 0xff
-
-    def write_bass(self, value):
-        data = (self.read_chip(2) | RDA_BASS)
-        if value == 0:
-            data = data ^ RDA_BASS
-        self.out_buffer[0] = data >> 8
-        self.out_buffer[1] = data & 0xff
-
-    def write_chan(self, value):
-        data = (self.read_chip(3) | RDA_CHAN) ^ RDA_CHAN
-        self.out_buffer[2] = (value >> 2) | (data >> 8)
-        self.out_buffer[3] = (value << 6) | (data & 0xff)
-
     def write_tune(self, value):
         data = ((self.out_buffer[2] << 8) | self.out_buffer[3]) | RDA_TUNE
-        if value == 0:
+        if not value:
             data = data ^ RDA_TUNE
         self.out_buffer[2] = data >> 8
         self.out_buffer[3] = data & 0xff
-
-    def write_de(self, value):
-        data = (self.read_chip(4) | RDA_DE) ^ RDA_DE
-        if value == 0:
-            data = data | RDA_50US
-        if value == 1:
-            data = data | RDA_75US
-        self.out_buffer[4] = data >> 8
-        self.out_buffer[5] = data & 0xff
-
-    def write_volume(self, value):
-        data = (self.read_chip(5) | RDA_VOLUME) ^ RDA_VOLUME
-        if value > RDA_VOLUME:
-            value = RDA_VOLUME
-        data = data | value
-        self.out_buffer[6] = data >> 8
-        self.out_buffer[7] = data & 0xff
 
     def write_from_chip(self):
         for loop in range(2, 8):
             data = self.read_chip(loop)
             self.out_buffer[(loop * 2) - 4] = data >> 8
             self.out_buffer[(loop * 2) - 3] = data & 0xff
-        self.write_tune(0)  # disable tuning
+        self.write_tune(False)  # disable tuning
+
+    WRITE_METHODS = {
+        "off": {"reg": 2, "mask": RDA_ENABLE, "how": "flag", "left-shift": 0},
+        "dmute": {"reg": 2, "mask": RDA_DMUTE, "how": "flag", "left-shift": 0},
+        "mono": {"reg": 2, "mask": RDA_MONO, "how": "flag", "left-shift": 0},
+        "bass": {"reg": 2, "mask": RDA_BASS, "how": "flag", "left-shift": 0},
+        "de": {"reg": 4, "mask": RDA_DE, "how": "value", "left-shift": 0},
+        "volume": {"reg": 5, "mask": RDA_VOLUME, "how": "value", "left-shift": 0},
+        "chan": {"reg": 3, "mask": RDA_CHAN, "how": "value", "left-shift": 6},
+    }
 
     READ_METHODS = {
         "dmute": {"reg": 2, "mask": RDA_DMUTE, "right-shift": 0},
@@ -236,21 +205,44 @@ class Rda5807m:
 
     def __getattr__(self, name):
         parts = name.split('_')
-        if len(parts) != 2 or parts[0] != "read":
+        if len(parts) != 2 or parts[0] not in ["read", "write"]:
             raise AttributeError("attribute '%s' not found" % (name,))
         name = parts[1]
-        if name not in self.READ_METHODS:
-            raise AttributeError("attribute '%s' not found" % (name,))
-        params = self.READ_METHODS[name]
-        return partial(
-            self.read_param,
-            params["reg"],
-            params["mask"],
-            params["right-shift"]
-        )
+        if parts[0] == "read":
+            if name in self.READ_METHODS:
+                params = self.READ_METHODS[name]
+                return partial(
+                    self.read_param,
+                    params["reg"],
+                    params["mask"],
+                    params["right-shift"]
+                )
+        elif parts[0] == "write":
+            if name in self.WRITE_METHODS:
+                params = self.WRITE_METHODS[name]
+                return partial(
+                    self.write_param,
+                    params["reg"],
+                    params["mask"],
+                    params["how"],
+                    params["left-shift"]
+                )
+        raise AttributeError("attribute '%s' not found" % (name,))
 
     def read_param(self, reg, mask, right_shift):
         return (self.read_chip(reg) & mask) >> right_shift
+
+    def write_param(self, reg, mask, how, left_shift, value):
+        data = (self.read_chip(reg) | mask) ^ mask
+        if how == "flag":
+            if value:
+                data = data | mask
+        elif how == "value":
+            value <<= left_shift
+            data = data | value
+        out_reg = (reg - 2) * 2
+        self.out_buffer[out_reg] = data >> 8
+        self.out_buffer[out_reg + 1] = data & 0xff
 
     def set_frequency(self, freq_request):
         data = self.read_band()
@@ -262,6 +254,7 @@ class Rda5807m:
             start_freq = 760
         elif data == RDA_65_76MHZ:
             start_freq = 650
+
         data = self.read_space()
         if data == RDA_200KHZ:
             spacing = 0
@@ -271,13 +264,15 @@ class Rda5807m:
             spacing = 2
         elif data == RDA_25KHZ:
             spacing = 4
+
         if spacing > 0:
             new_freq = (freq_request - start_freq) * spacing
         else:
             new_freq = int((freq_request - start_freq) / 2)
-        self.write_dmute(1)
+
+        self.write_dmute(True)
         self.write_chan(new_freq)
-        self.write_tune(1)
+        self.write_tune(True)
         self.write_chip(4)
 
     def on(self):
@@ -285,15 +280,11 @@ class Rda5807m:
         self.write_chip(12)
 
     def off(self):
-        self.write_off()
+        self.write_off(False)
         self.write_chip(2)
 
     def set_mute(self, mute):
-        if mute:
-            self.write_dmute(0)
-            self.write_chip(2)
-        else:
-            self.write_dmute(1)
+            self.write_dmute(not mute)
             self.write_chip(2)
 
     def set_volume(self, volume):
@@ -302,29 +293,20 @@ class Rda5807m:
         self.write_chip(8)
 
     def set_bass(self, bass):
-        if bass:
-            self.write_bass(1)
-            self.write_chip(2)
-        else:
-            self.write_bass(0)
+            self.write_bass(bass)
             self.write_chip(2)
 
     def set_stereo(self, stereo):
-        if stereo:
-            self.write_mono(0)
-            self.write_chip(2)
-        else:
-            self.write_mono(1)
+            self.write_mono(not stereo)
             self.write_chip(2)
 
     def set_deemphasis(self, deemphasis):
         self.write_from_chip()
-        if deemphasis == 75:
-            self.write_de(1)
-            self.write_chip(6)
+        if deemphasis == 50:
+            self.write_de(RDA_50US)
         else:
-            self.write_de(0)
-            self.write_chip(6)
+            self.write_de(RDA_75US)
+        self.write_chip(6)
 
     def close(self):
         self.pi.i2c_close(self.read_handle)
